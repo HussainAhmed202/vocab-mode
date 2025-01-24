72fb2d25341672bc2ae6db3fc3dad568ee3efc37
HussainAhmed202 <h2002ahmed@gmail.com>
2025-01-24 17:55:58 +0500

    added narrative design manager

---
+++ Assets/SendMessage.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SendMessage : MonoBehaviour
+{
+ void Message()
+    {
+        // Message using rich text.
+        Debug.Log("Entered the teleport target");
+    }
+}
