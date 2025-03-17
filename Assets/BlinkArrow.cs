603c0d21cc580764d49012d8f5a7a3c27d6453b6
HussainAhmed202 <h2002ahmed@gmail.com>
2025-03-17 10:09:16 +0500

    added indicator arrow to pizza section with blinking logic

---
+++ Assets/BlinkArrow.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class BlinkArrow : MonoBehaviour
+{
+
+    public float blinkInterval = 0.5f; // Time interval for blinking
+
+    public SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer component
+
+
+    void Start()
+    {
+        
+        if (spriteRenderer == null)
+        {
+            // If the SpriteRenderer is not assigned, try to get it from the same GameObject
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (spriteRenderer != null)
+        {
+            // Start the blinking coroutine
+            StartCoroutine(Blink());
+        }
+        else
+        {
+            Debug.LogError("No SpriteRenderer component found!");
+        }
+        
+    }
+
+    private System.Collections.IEnumerator Blink()
+    {
+        while (true)
+        {
+            // Toggle the visibility of the sprite
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+
+            // Wait for the specified interval before the next blink
+            yield return new WaitForSeconds(blinkInterval);
+        }
+    }
+}
