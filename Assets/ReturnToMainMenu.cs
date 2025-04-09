78e94887b1c7d6eee294823ba7448b4538768d26
HussainAhmed202 <h2002ahmed@gmail.com>
2025-04-09 15:47:43 +0500

    added main menu scene preloading for faster transistion

--- Assets/ReturnToMainMenu.cs
+++ Assets/ReturnToMainMenu.cs
@@ -3,23 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class ReturnToMainMenu : MonoBehaviour
 {
+
+    private AsyncOperation preloadOperation;
+
+    void Start()
+    {
+        //  Preload main menu for faster transisition
+        preloadOperation = SceneManager.LoadSceneAsync("MainMenu");
+        preloadOperation.allowSceneActivation = false; // Don't switch yet
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Returning to main menu...");
-            StartCoroutine(LoadMainMenuAsync());
-        }
-    }
+            preloadOperation.allowSceneActivation = true; // Now activate it
 
-    private System.Collections.IEnumerator LoadMainMenuAsync()
-    {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMenu");
-
-        while (!asyncLoad.isDone)
-        {
-            // Optionally, add a loading screen or progress bar update here
-            yield return null;
         }
     }
 }
