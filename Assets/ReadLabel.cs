92f1bc5195996631d5a5410e736a4622cdf006db
HussainAhmed202 <h2002ahmed@gmail.com>
2025-04-23 11:03:29 +0500

    removed the commented out section of the code

--- Assets/ReadLabel.cs
+++ Assets/ReadLabel.cs
@@ -7,29 +7,6 @@ public class ReadLabel : MonoBehaviour
     [SerializeField]
     private TooltipFacade tooltipObject;
 
-    // private void Start()
-    // {
-    //     if (tooltipObject == null)
-    //     {
-    //         Debug.LogError("Tooltip object is not assigned.");
-    //         return;
-    //     }
-    
-
-    //     // tooltip = tooltipObject.GetComponent<TooltipFacade>();
-
-    //     // if (tooltip == null)
-    //     // {
-    //     //     Debug.LogError("TooltipFacade component not found on the assigned GameObject.");
-    //     //     return;
-    //     // }
-
-    
-    //     string tooltipText = tooltipObject.TooltipText;
-    //     Debug.Log("Tooltip Text: " + tooltipText);
-    // }
-
-
     public void GetTooltipText()
     {
         if (tooltipObject != null)
