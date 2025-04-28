b7a0114965fa05bf6083f027d2f964c098e98baf
HussainAhmed202 <h2002ahmed@gmail.com>
2025-04-28 15:56:06 +0500

    added volume button which is click button. changed its default mesh to a image component which greatly simplifies things

--- Assets/VolumeButtonHandler.cs
+++ Assets/VolumeButtonHandler.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 public class VolumeButtonHandler : MonoBehaviour
 {
     public GameObject screenPanel;
-    public Button volumeOffButton;
 
     public void OnVolumeOffClicked()
     {
